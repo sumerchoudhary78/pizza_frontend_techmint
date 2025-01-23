2bf172fc200bd86bc0ad41679c26e1cf53075abe
Sumer Choudhary <115416306+sumerchoudhary78@users.noreply.github.com>
2025-01-23 15:23:09 +0530

    Update store.js

--- store/store.js
+++ store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pizzaReducer from '@/store/features/PizzaSlice';
- // Ensure this path is correct
+ 
 
 export const store = configureStore({
   reducer: {
